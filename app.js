a4a072d29689d0fb139638cf418f9f20accf2bcc
jenifercoila <jeniferfcr@gmail.com>
2019-02-05 21:22:00 -0500

    Commit dos de mi prueba para publicar post publicar post, uso la data que me publica en ese momento, deberia usar la data de firestore

--- app.js
+++ app.js
@@ -16,17 +16,23 @@ let contenido = document.getElementById('content-field');
 let posteado = document.getElementById('posted');
 const container = document.getElementById('container');
 
+/* Funcion para crear documentos en mi database */
 document.getElementById('add').addEventListener('click', () => {
     console.log('entro a mi funcion de click');
+    /*  Creo un documento en mi coleccion 'posts'*/
     db.collection("posts").add({
+    /*  Le asigno el value de mi textarea*/
         content: contenido.value,
     })
+    /*  Entonces mi promesa me dice*/
     .then(docRef => {
         console.log('escritura exitosa');
         console.log("Document written with ID: ", docRef.id)
+        /*  Si el post está vacio le mando un alert*/
         if (contenido.value === '') {
-        document.getElementById('add').disabled = true;
+        alert('Por favor publica algo');
         } else {
+    /*  En su defecto invoco la funcion que está más abajo*/
         creacion(docRef.id);
         }
     })
@@ -39,18 +45,25 @@ document.getElementById('add').addEventListener('click', () => {
 
 
 const creacion = (id) => {
-
+    /*   Creo una nueva etiqueta p */
     let newPost = document.createElement('p');
+    /*  Le asigno un aributo*/
     newPost.setAttribute('id', id);
     newPost.innerHTML= contenido.value;
+    /*  Añado mi etiqueta p a mi container que está en mi HTML*/
     container.appendChild(newPost);
+    /*   Creo mi boton para eliminar */
     let button = document.createElement('input');
+     /*  Agrego atributos que quiero (aun no estoy segura que tan util sea 
+        ponerle un id+button pero los puse porque se me ocurrio que seria util) */
     button.setAttribute('type', 'button')
     button.setAttribute('value', 'Eliminar')
     button.setAttribute('id', id+'button')
+    /*   Añado mi boton Eliminar a mi container */
     newPost.appendChild(button);
 }
 
+/* Esto aun no está funcional*/
 document.getElementById(id +'button').addEventListener('click', () => {
    
 db.collection("post").doc(docref.id).delete().then(() => {
